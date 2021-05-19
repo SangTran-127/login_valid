0f1668b573cdbf7e3f6c3546817976e049142629
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-05-19 13:32:08 +0700

    update2

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, {useState} from "react"
 import Login from "./Login"
+import Register from "./Register"
 function App() {
   return <>
-    <Login />
+    <Register />
   </>
 }
 
